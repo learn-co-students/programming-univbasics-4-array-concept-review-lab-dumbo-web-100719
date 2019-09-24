2683d501c5c5dda7875f587d7208c9b8188bdca0
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-24 22:52:07 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,41 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.length.times do |count|
+    if array[count] == value_to_find
+      return count
+    	end
+    end
+  nil
 end
 
+#def find_max_value(array)
+   #array.length.times do |index|
+  #puts array[index]
+  
+  #counter = 1 
+ # if array[index] < counter
+   # += counter
+  #else
+   # return counter
+#end
+#end
+
+
 def find_max_value(array)
-  # Add your solution here
+  placeholder = 0
+  array.length.times do |index|
+    if array[index] > placeholder
+      placeholder = array[index]
+   end
+  end
+  return placeholder
 end
 
 def find_min_value(array)
-  # Add your solution here
+ placeholder = 100000000
+  array.length.times do |index|
+    if array[index] < placeholder
+      placeholder = array[index]
+   end
+  end
+  return placeholder
 end
